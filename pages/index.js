ec23e6ae0ecfc6da656133135333399d4c89f341
Henrique Ulbrich <henrique1_souza@hotmail.com>
2024-03-19 10:47:48 -0300

    adicionar index

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function home() {
+    return (<div>
+        <h1>home</h1>
+
+        <link href="/sobre">
+            <a>acessar pagina sobre</a>
+            </link>
+    </div>)
+}
+
+export default home
