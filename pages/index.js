f06bf91d72e087e6e39b9e85a2f8b40b8a9aad13
Henrique Ulbrich <henrique1_souza@hotmail.com>
2024-03-19 11:01:55 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-//import Link from 'next/link';
+
 
 function home() {
     return (<div>
         <h1>home</h1>
 
-        <link href="/sobre">
-            <a>acessar pagina sobre</a>
-            </link>
+    
     </div>)
 }
 
